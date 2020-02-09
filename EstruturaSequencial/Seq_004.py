8311d7ab69ad5b2bde38a201aa1d02741c0b3d26
Edicley Henrique <edyhenrique@gmail.com>
2020-02-09 17:34:57 -0300

    Exercício sequencial_04 concluído.

---
+++ EstruturaSequencial/Seq_004.py
@@ -0,0 +1,10 @@
+print("Informe as suas quatro notas e lhe direi a sua média.")
+
+n1 = float(input("Informe sua 1ª nota: "))
+n2 = float(input("Informe sua 2ª nota: "))
+n3 = float(input("Informe sua 3ª nota: "))
+n4 = float(input("Informe sua 4ª nota: "))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print("Sua médial foi:" ,media)
