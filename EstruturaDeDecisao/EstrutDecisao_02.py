e2aae2c724240fc6df3e8abc231570f7e384bd6e
Edicley Henrique <edyhenrique@gmail.com>
2020-02-12 20:24:23 -0300

    EstrutDecisao_02 resolvido.

---
+++ EstruturaDeDecisao/EstrutDecisao_02.py
@@ -0,0 +1,8 @@
+#Verificando valor positivo ou negativo.
+
+n = int(input("Informe um número e direi se ele é positivo ou negativo: "))
+
+if n > 0:
+    print("Seu número é positivo!")
+else:
+    print("Seu número é negativo!")
