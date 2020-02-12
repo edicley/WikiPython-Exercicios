f2b45a56fcefa20af3371d85d2f7ccf5b49c5938
Edicley Henrique <edyhenrique@gmail.com>
2020-02-12 20:19:58 -0300

    EstrutDecisao_01 resolvido.

--- EstruturaDeDecisao/EstrutDecisao_01.py
+++ EstruturaDeDecisao/EstrutDecisao_01.py
@@ -1,1 +1,9 @@
-print('oi')
+n1 = int(input("Informe o 1º número: "))
+n2 = int(input("Informe o 2º número: "))
+
+if n1 > n2:
+    print(n1)
+elif n2 > n1:
+    print(n2)
+else:
+    print("Os números são iguais.")
