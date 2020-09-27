0a269b209c821ad2b3dfd9a481ebde27edf9bf11
Edicley <edyhenrique@gmail.com>
2020-09-27 13:13:27 -0300

    Adicionando enunciado e refatorando.

--- EstruturaDeDecisao/EstrutDecisao_14.py
+++ EstruturaDeDecisao/EstrutDecisao_14.py
@@ -1,18 +1,33 @@
+"""
+Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule
+a sua média. A atribuição de conceitos obedece à tabela abaixo:
+
+  Média de Aproveitamento  Conceito
+  Entre 9.0 e 10.0            A
+  Entre 7.5 e 9.0             B
+  Entre 6.0 e 7.5             C
+  Entre 4.0 e 6.0             D
+  Entre 4.0 e zero            E
+
+O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito
+for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+"""
+
 print("Informe suas duas notas, calcularemos a média e exibiremos o conceito obtido.")
 n1 = float(input("Informe a primeira nota: "))
 n2 = float(input("Informe a segunda nota: "))
 
 media = (n1 + n2) / 2
 conceito = ''
-if media >= 9.0 and media <= 10.0:
+if 9.0 <= media <= 10.0:
     conceito = 'A'
-elif media >= 7.5 and media < 9.0:
+elif 7.5 <= media < 9.0:
     conceito = 'B'
-elif media >= 6 and media < 7.0:
+elif 6 <= media < 7.0:
     conceito = 'C'
-elif media >= 4 and media < 6.0:
+elif 4 <= media < 6.0:
     conceito = 'D'
-elif media < 4 and media >= 0:
+elif 4 > media >= 0:
     conceito = 'E'
 
 if conceito == 'A' or conceito == 'B' or conceito == 'C':
