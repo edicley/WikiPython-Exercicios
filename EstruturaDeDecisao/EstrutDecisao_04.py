b4e626f7aa92914733025ebb55f92182ad1e563c
Edicley <edyhenrique@gmail.com>
2020-09-27 13:10:57 -0300

    Refatorando.

--- EstruturaDeDecisao/EstrutDecisao_04.py
+++ EstruturaDeDecisao/EstrutDecisao_04.py
@@ -1,6 +1,6 @@
-'''
+"""
 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-'''
+"""
 
 print('Informe as suas quatro notas do bimestre e calcularemos a média.')
 nota_01 = float(input('Informe a primeira nota: '))
@@ -11,4 +11,3 @@ nota_04 = float(input('Informe a quarta nota: '))
 media = (nota_01 + nota_02 + nota_03 + nota_04) / 4
 
 print(f'Sua média foi: {media}')
-
