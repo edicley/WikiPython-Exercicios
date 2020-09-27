b8ae44ad8159e3eace7e56553bed0f972a54792d
Edicley <edyhenrique@gmail.com>
2020-09-27 13:12:50 -0300

    Adicionando enunciado e refatorando.

--- EstruturaDeDecisao/EstrutDecisao_12.py
+++ EstruturaDeDecisao/EstrutDecisao_12.py
@@ -1,3 +1,25 @@
+"""
+Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda,
+que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário
+Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os
+descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+
+Desconto do IR:
+Salário Bruto até 900 (inclusive) - isento
+Salário Bruto até 1500 (inclusive) - desconto de 5%
+Salário Bruto até 2500 (inclusive) - desconto de 10%
+Salário Bruto acima de 2500 - desconto de 20%
+
+Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade de
+hora é 220.
+
+        Salário Bruto: (5 * 220)        : R$ 1100,00
+        (-) IR (5%)                     : R$   55,00
+        (-) INSS ( 10%)                 : R$  110,00
+        FGTS (11%)                      : R$  121,00
+        Total de descontos              : R$  165,00
+        Salário Liquido                 : R$  935,00
+"""
 
 hora = float(input("Qual o valor da sua hora de trabalho? "))
 total_horas = int(input("Qual sua carga horária mensal?"))
@@ -5,14 +27,14 @@ salario_bruto = hora * total_horas
 
 if salario_bruto <= 900:
     ir = 0
-elif salario_bruto > 900 and salario_bruto <= 1500:
+elif 900 < salario_bruto <= 1500:
     ir = 5
-elif salario_bruto > 1500 and salario_bruto <= 2500:
+elif 1500 < salario_bruto <= 2500:
     ir = 10
 else:
     ir = 20
     
-valor_ir = salario_bruto * ir /100
+valor_ir = salario_bruto * ir / 100
 inss = 10
 fgts = 11
 sindicado = 3
