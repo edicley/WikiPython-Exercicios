5533df27e1f6aa3bd046cbcc6d8381a9d0b725f0
Edicley <edyhenrique@gmail.com>
2020-09-27 13:13:05 -0300

    Adicionando enunciado.

--- EstruturaDeDecisao/EstrutDecisao_13.py
+++ EstruturaDeDecisao/EstrutDecisao_13.py
@@ -1,3 +1,8 @@
+"""
+Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar
+outro valor deve aparecer valor inválido.
+"""
+
 dia = int(input("De 1 à 7, informe o número e lhe direi o dia da semana."))
 
 if dia == 1:
