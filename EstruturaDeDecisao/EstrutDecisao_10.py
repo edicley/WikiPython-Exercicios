c9f6944a276b8b96be18e04bf86a61bb316cac57
Edicley <edyhenrique@gmail.com>
2020-09-27 13:12:12 -0300

    Refatorando.

--- EstruturaDeDecisao/EstrutDecisao_10.py
+++ EstruturaDeDecisao/EstrutDecisao_10.py
@@ -1,6 +1,8 @@
-# Faça um Programa que pergunte em que turno você estuda. 
-# Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
-# Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+"""
+Faça um Programa que pergunte em que turno você estuda.
+Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+"""
 
 turno = input("Olá estudante, qual o seu turno? Responda M para Matutino, V para Vespertino e N para Noturno.")
 
