a860caac3624e95d4c025c8fcbd7d3c69a7a76ca
Edicley <edyhenrique@gmail.com>
2020-09-27 13:13:42 -0300

    Adicionando enunciado.

--- EstruturaDeDecisao/EstrutDecisao_15.py
+++ EstruturaDeDecisao/EstrutDecisao_15.py
@@ -1,3 +1,13 @@
+"""
+Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo.
+Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+Dicas:
+
+Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+Triângulo Equilátero: três lados iguais;
+Triângulo Isósceles: quaisquer dois lados iguais;
+Triângulo Escaleno: três lados diferentes;
+"""
 print("Informe três valores e lhe direi se é possível criar um triângulo. Se for possível, lhe direi a categoria dele.")
 l1 = float(input("Informe o primeiro valor: "))
 l2 = float(input("Informe o segundo valor: "))
@@ -13,4 +23,3 @@ if l1 + l2 > l3 and l1 + l3 > l2 and l2 + l3 > l1:
         print("Temos um triângulo Escaleno.")
 else:
     print("Não podemos formar um triângulo com esses valores.")
-    
