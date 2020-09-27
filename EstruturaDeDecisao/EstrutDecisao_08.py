340ae836f9b32100d696f8aa42e7b3c051605cc0
Edicley <edyhenrique@gmail.com>
2020-09-27 13:11:49 -0300

    Refatorando.

--- EstruturaDeDecisao/EstrutDecisao_08.py
+++ EstruturaDeDecisao/EstrutDecisao_08.py
@@ -1,6 +1,7 @@
-# Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, 
-# sabendo que a decisão é sempre pelo mais barato.
-
+"""
+Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,
+sabendo que a decisão é sempre pelo mais barato.
+"""
 prod1 = int(input("Qual o preço do quiejo coalho? "))
 prod2 = int(input("Qual o preço do quiejo mussarela? "))
 prod3 = int(input("Qual o preço do quiejo prato? "))
